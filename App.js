da17f5fd43c2a3d7caa61335b0a00ca8602c2bef
Darbaz Ali <darbaz.a.h@gmail.com>
2020-07-14 09:21:33 +0300

    fetch data wit fetch api

--- App.js
+++ App.js
@@ -10,3 +10,42 @@ container.append('h2')
 container.append('p')
     .attr('id', 'description')
     .text("1753 - 2015: base temperature 8.66℃")
+
+
+
+const drawHeatMap = data => {
+
+    // {
+    //     "baseTemperature": 8.66,
+    //     "monthlyVariance": [
+    //       {
+    //         "year": 1753,
+    //         "month": 1,
+    //         "variance": -1.366
+    //       },
+    //       {
+    //         "year": 1753,
+    //         "month": 2,
+    //         "variance": -2.223
+    //       },
+    //     }
+
+
+    
+
+
+}
+
+
+
+const api_url = "https://raw.githubusercontent.com/freeCodeCamp/ProjectReferenceData/master/global-temperature.json";
+
+
+// fetch data 
+fetch( api_url )
+.then( res => res.json())
+.then( json => {
+    const data = json["monthlyVariance"];
+
+    drawHeatMap(data)
+})
