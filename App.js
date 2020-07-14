2057f0bc3f940b6f2f095804436335cab1dba140
Darbaz Ali <darbaz.a.h@gmail.com>
2020-07-14 09:28:24 +0300

    create canvas

--- App.js
+++ App.js
@@ -11,6 +11,31 @@ container.append('p')
     .attr('id', 'description')
     .text("1753 - 2015: base temperature 8.66℃")
 
+// margins
+const margin = {
+    top: 20,
+    left: 20,
+    bottom: 20,
+    right: 50
+}
+
+// dimentions
+const width = 800 - margin.left - margin.right;
+const height = 400 - margin.top - margin.bottom;
+
+
+
+// create canvas
+// canvas is the main container for all svg elements
+const canvas = container.append('svg')
+    .attr('viewBox', `0 0 ${width + margin.left + margin.right} ${hight + margin.top + margin.bottom}`);
+
+
+// Create Group Container
+const group = canvas.append('g')
+    .attr('transform', `translate(${margin.left}, ${margin.top})`)
+
+
 
 
 const drawHeatMap = data => {
@@ -31,7 +56,7 @@ const drawHeatMap = data => {
     //     }
 
 
-    
+
 
 
 }
